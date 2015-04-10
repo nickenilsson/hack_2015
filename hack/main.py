da0020f00a55f6b65e05fb9b30143f2554e575bc
Niklas NIlsson <nilsson.sthlm@gmail.com>
2015-04-10 16:00:30 +0200

    Added sounds. Buggy

--- hack/main.py
+++ hack/main.py
@@ -6,14 +6,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import sys
 sys.path.insert(0, "lib")
 import Leap
+import pyglet
+
 
 import time
 
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture, Vector
 
-
-
+x_chord_line_left = -50
+x_chord_line_right = 50
 y_strumming_line = 200
+
 
 class SampleListener(Leap.Listener):
     def on_connect(self, controller):
@@ -30,13 +33,22 @@ class SampleListener(Leap.Listener):
         previous_y = previous_frame.hands.rightmost.palm_position.y
         if ((previous_y > y_strumming_line) and (right_hand.palm_position.y < y_strumming_line)) or ((previous_y < y_strumming_line) and (right_hand.palm_position.y > y_strumming_line)):
             print "STRUM"
-            print "Force: %s" % right_hand.palm_velocity.y
-            time.sleep(.1)
+            left_hand = hands.leftmost
+            pitch = left_hand.palm_position.x
 
-        left_hand = hands.leftmost
-        pitch = left_hand.palm_position.x
-        print "Tone : %s" % pitch
-        print "Frame available"
+            if pitch > x_chord_line_right:
+                file_path = "c.wav"
+            elif pitch < x_chord_line_left:
+                file_path = "d.wav"
+            else:
+                file_path = "e.wav"
+            # print "Force: %s" % right_hand.palm_velocity.y
+            music = pyglet.resource.media(file_path)
+            music.play()
+            pyglet.app.run()
+
+        # print "Tone : %s" % pitch
+        # print "Frame available"
 
 def main():
 
@@ -46,11 +58,6 @@ def main():
 
     # Have the sample listener receive events from the controller
     controller.add_listener(listener)
-    controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
-    controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
-
-    controller.config.set("Gesture.Swipe.MinVelocity", 500)
-
 
     print "Press Enter to quit..."
     try:
